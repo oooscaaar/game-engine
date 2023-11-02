70e94187f52160fce79434bc750c2e44407d677c
Oscar M. <empanaisimo@gmail.com>
2023-11-02 17:25:06 +0100

    Finish render triangle

--- Source/ModuleOpenGL.cpp
+++ Source/ModuleOpenGL.cpp
@@ -50,11 +50,12 @@ bool ModuleOpenGL::Init()
 	//CreateTriangleVBO
 	float vtx_data[] = { -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
 	glGenBuffers(1, &vbo);
-	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); // set vbo active
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vtx_data), vtx_data, GL_STATIC_DRAW);
+
 	// Create program
-	const char* vtx_path = "hello_world.vert";
-	unsigned vtx_shader = App->GetProgram()->CompileShader(GL_VERTEX_SHADER, App->GetProgram()->ReadShader(vtx_path));
-	unsigned frg_shader = App->GetProgram()->CompileShader(GL_FRAGMENT_SHADER, App->GetProgram()->ReadShader("hello_world.frag"));
+	unsigned vtx_shader = App->GetProgram()->CompileShader(GL_VERTEX_SHADER, App->GetProgram()->ReadShader("../Source/shaders/hello_world.vert"));
+	unsigned frg_shader = App->GetProgram()->CompileShader(GL_FRAGMENT_SHADER, App->GetProgram()->ReadShader("../Source/shaders/hello_world.frag"));
 	program = App->GetProgram()->CreateProgram(vtx_shader, frg_shader);
 
 	return true;
@@ -66,16 +67,10 @@ update_status ModuleOpenGL::PreUpdate()
 	int windowCurrentH;
 	SDL_GetWindowSize(App->GetWindow()->window, &windowCurrentW, &windowCurrentH);
 	glViewport(0, 0, windowCurrentW, windowCurrentH);
-	// glClearColor(0.1f, 0.1f, 0.1f, 1.0f); // Paint in RED :D
-	// glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.1f, 1.f, 0.1f, 1.0f); // Paint in RED :D
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// TODO: Extract this logic
-	// Render VBO Logic
-	glBindBuffer(GL_ARRAY_BUFFER, vbo); // set vbo active 
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // size = 3 float per vertex | stride = 0 is equivalent to stride = sizeof(float)*3 
-	glUseProgram(program);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	return UPDATE_CONTINUE;
 }
@@ -83,6 +78,13 @@ update_status ModuleOpenGL::PreUpdate()
 // Called every draw update
 update_status ModuleOpenGL::Update()
 {
+	// Render VBO Logic
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); // set vbo active 
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); // size = 3 float per vertex | stride = 0 is equivalent to stride = sizeof(float)*3 
+	glUseProgram(program);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	return UPDATE_CONTINUE;
 }
 
